cfc7b92f3ebf1783abdcc04ed7cac1fb9fdc2d0f
Danila <dyukov_danila@mail.ru>
2025-10-27 23:49:13 +0300

    Добавлена обработка даты и отображение добавленных задач

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+function normalDate(date) {
+    if (new Date() >= new Date(date)) {
+        return false;
+    }
+    return true;
+}
 const nameTaskInput = document.querySelector('.taskNameInput');
 const prioritySelect = document.querySelector('.prioritySelect');
 const typeSelect = document.querySelector('.typeSelect');
 const dateInput = document.querySelector('.dateInput');
 const addTaskButton = document.querySelector('.addTaskButton');
+const taskListContainer = document.querySelector('.tasksListContent');
 const taskList = document.querySelector('.taskList');
 let tasks = [];
 
@@ -11,9 +18,14 @@ addTaskButton.addEventListener('click', () => {
     let priorityValue = prioritySelect.value;
     let typeValue = typeSelect.value;
     let dateValue = dateInput.value;
+    taskList.style.display = 'flex';
 
     if (nameTaskValue === '' || dateValue === '') {
         alert('Please fill in all fields.');
+        return;
+    }
+    if (!normalDate(dateValue)) {
+        alert('Please select a valid future date.');
         return;
     }
     let newTask = {
@@ -24,8 +36,30 @@ addTaskButton.addEventListener('click', () => {
         "completed": false
     };
     tasks.push(newTask);
-    // showAllTasks(); метод не реализован
+    showAllTasks();
     nameTaskInput.value = '';
     dateInput.value = '';
-
+    console.log("Task added:", newTask);
 });
+const showTask = (task, index) => {
+    return `
+    <div class="taskItem" style="background-color: ${task.completed ? '#aaf87cff' : '#f87c7cff'}">
+        <h3>${index + 1}. ${task.name}</h3>
+        <p>Priority: ${task.priority}</p>
+        <p>Type: ${task.type}</p>
+        <p>Date: ${task.date}</p>
+        <p>Status: ${task.completed ? 'Complete' : 'Not complete'}</p>
+        <div class="buttonTaskList">
+            <button class="taskButton" onclick="delTask(${index})">Delete</button>
+            <button class="taskButton" onclick="completeTask(${index})">Complete</button>
+        </div>
+    </div>
+    `;
+};
+const showAllTasks = () => {
+    let taskListHTML = '';
+    tasks.map((task, index) => {
+        taskListHTML += showTask(task, index);
+    });
+    taskListContainer.innerHTML = taskListHTML;
+};
